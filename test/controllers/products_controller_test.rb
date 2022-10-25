6f82107403a5920a698718bf59e32c2c77afb62a
burgosibarra <burgosibarra@uc.cl>
2022-10-25 17:34:25 -0300

    some test were corrected, 100% still pending

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,29 +3,38 @@
 require 'test_helper'
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @product = Product.create(name: "Coca Cola", price: 1500, category: "drink", size: 500)
+  end
+
+  def teardown
+    Product.destroy_all
+  end
+
   test 'should get new' do
     get products_new_url
     assert_response :success
   end
 
   test 'should get create' do
-    get products_create_url
-    assert_response :success
+    post products_create_url, params: {name: "Coca Cola Light", price: 2000, category: "drink", size: 500}
+    assert_response :redirect
   end
 
   test 'should get update' do
-    get products_update_url
-    assert_response :success
+    put products_update_url(@product.id), params: {name: "Coca Cola", price: 1750, category: "drink", size: 500}
+    assert_response :redirect
   end
 
   test 'should get edit' do
-    get products_edit_url
+    get products_edit_url(@product.id)
     assert_response :success
   end
 
   test 'should get destroy' do
-    get products_destroy_url
-    assert_response :success
+    delete products_destroy_url(@product.id)
+    assert_response :redirect
   end
 
   test 'should get index' do
@@ -33,8 +42,4 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test 'should get show' do
-    get products_show_url
-    assert_response :success
-  end
 end
