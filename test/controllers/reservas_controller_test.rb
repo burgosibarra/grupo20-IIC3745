159e842a1dcb2861f579d37f09adc9912f4fe3c0
burgosibarra <burgosibarra@uc.cl>
2022-10-25 21:38:27 -0300

    100% test were created, but 1 still failing

--- test/controllers/reservas_controller_test.rb
+++ test/controllers/reservas_controller_test.rb
@@ -4,10 +4,10 @@ require 'test_helper'
 
 class ReservasControllerTest < ActionDispatch::IntegrationTest
   def setup
-    movie = Movie.create(title: 'Matrix')
-    MovieTime.create(room: 5, date_start: Date.new(2000, 11, 10),
-                     date_end: Date.new(2000, 11, 12), time: 'TANDA',
-                     movie_id: movie.id)
+    @movie = Movie.create(title: 'Matrix')
+    @movie_time = MovieTime.create(room: 5, date_start: Date.new(2000, 11, 10),
+                                   date_end: Date.new(2000, 11, 12), time: 'TANDA',
+                                   movie_id: @movie.id)
   end
 
   def teardown
@@ -15,9 +15,79 @@ class ReservasControllerTest < ActionDispatch::IntegrationTest
     MovieTime.destroy_all
     Movie.destroy_all
   end
-  test 'Posting a new reserva' do
-    assert_difference 'Reserva.count' do
+
+  test 'get new' do
+    get new_reserva_url(@movie_time.room, '2000-11-12', @movie_time.time)
+    assert_response :success
+  end
+
+  test 'Redirect when post a new reserva with valid params' do
+    post new_reserva_url(5, '2000-11-12', 'TANDA'),
+         params: { reservation_seats: 'C-3, C-10', name: 'Diego' }
+    assert_response :redirect
+  end
+
+  test 'Should create a new reserva when valid params are given' do
+    assert_difference 'Reserva.count', 2 do
       post new_reserva_url(5, '2000-11-12', 'TANDA'),
+           params: { reservation_seats: 'C-3, C-10', name: 'Diego' }
+    end
+  end
+
+  test 'Redirect when post a new reserva without name' do
+    post new_reserva_url(5, '2000-11-12', 'TANDA'),
+         params: { reservation_seats: 'C-3' }
+    assert_response :redirect
+  end
+
+  test 'Should not create a new reserva when name was not given' do
+    assert_difference 'Reserva.count', 0 do
+      post new_reserva_url(5, '2000-11-12', 'TANDA'),
+           params: { reservation_seats: 'C-3, C-10' }
+    end
+  end
+
+  test 'Redirect when post a new reserva without seat' do
+    post new_reserva_url(5, '2000-11-12', 'TANDA'),
+         params: { reservation_seats: '', name: 'Diego' }
+    assert_response :redirect
+  end
+
+  test 'Should not create a new reserva when seat was not given' do
+    assert_difference 'Reserva.count', 0 do
+      post new_reserva_url(5, '2000-11-12', 'TANDA'),
+           params: { reservation_seats: '', name: 'Diego' }
+    end
+  end
+
+  test 'Redirect when post a new reserva that was already taken' do
+    Reserva.create(sala: 5, fecha: Date.new(2000, 11, 12),
+                   asiento: 39, horario: 'TANDA',
+                   name: 'Diego1')
+    post new_reserva_url(5, '2000-11-12', 'TANDA'),
+         params: { reservation_seats: 'C-3', name: 'Diego2' }
+    assert_response :redirect
+  end
+
+  test 'Should not create a new reserva when seat was already taken' do
+    Reserva.create(sala: 5, fecha: Date.new(2000, 11, 12),
+                   asiento: 39, horario: 'TANDA',
+                   name: 'Diego1')
+    assert_difference 'Reserva.count', 1 do
+      post new_reserva_url(5, '2000-11-12', 'TANDA'),
+           params: { reservation_seats: 'C-3', name: 'Diego2' }
+    end
+  end
+
+  test 'Redirect when post a new reserva with invalid params' do
+    post new_reserva_url(5, '2000-11-12', 'MATINÉ'),
+         params: { reservation_seats: 'C-3', name: 'Diego' }
+    assert_response :redirect
+  end
+
+  test 'Should not create a new reserva with invalid params' do
+    assert_difference 'Reserva.count', 0 do
+      post new_reserva_url(5, '2000-11-12', 'MATINÉ'),
            params: { reservation_seats: 'C-3', name: 'Diego' }
     end
   end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -3,7 +3,27 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def teardown
+    Product.destroy_all
+  end
+
+  test 'Product with valid params' do
+    product = Product.create(name: 'Coca Cola', price: 1500, category: 'drink', size: 500)
+    assert_equal(true, product.valid?)
+  end
+
+  test 'Product with invalid params' do
+    product = Product.create(name: 'Coca Cola', price: -1500, category: 'drink', size: -500)
+    assert_equal(false, product.valid?)
+  end
+
+  test 'Drink product without size' do
+    product = Product.create(name: 'Coca Cola', price: -1500, category: 'drink')
+    assert_equal(false, product.valid?)
+  end
+
+  test 'Food product without size' do
+    product = Product.create(name: 'Coca Cola', price: -1500, category: 'food')
+    assert_equal(false, product.valid?)
+  end
 end
