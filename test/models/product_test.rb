b01f2ff0a99e3bbece21f29f4753e7411f35300d
Raúl Ignacio Diaz Campos <ridiaz2@uc.cl>
2022-10-28 21:32:22 -0300

    feat: add price and size test

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -26,4 +26,34 @@ class ProductTest < ActiveSupport::TestCase
     product = Product.new(name: 'Coca Cola', price: -1500, category: 'food')
     assert_equal(false, product.valid?)
   end
+
+  test 'Food product size as string' do
+    product = Product.new(name: 'Cabritas', price: 1000, category: 'food', size: 100)
+    assert_equal('100.0 gr', product.size_string)
+  end
+
+  test 'Drink product size as string' do
+    product = Product.new(name: 'Jugo', price: 1000, category: 'drink', size: 100)
+    assert_equal('100.0 cc', product.size_string)
+  end
+
+  test 'Souvenir product size as string' do
+    product = Product.new(name: '', price: 1000, category: 'souvenir')
+    assert_equal('', product.size_string)
+  end
+
+  test 'Product price with 9 digits as string' do
+    product = Product.new(name: 'Cabritas', price: 123_456_789, category: 'food', size: 100)
+    assert_equal('$123.456.789', product.price_string)
+  end
+
+  test 'Product price with 3 digits' do
+    product = Product.new(name: 'Jugo', price: 123, category: 'drink', size: 100)
+    assert_equal('$123', product.price_string)
+  end
+
+  test 'product price with 4 digits as string' do
+    product = Product.new(name: '', price: 1000, category: 'souvenir')
+    assert_equal('$1.000', product.price_string)
+  end
 end
